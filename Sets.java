e60fcb218627360656cdbfb35cfd5b48697e16cd
Pato-gt <ordopatrick@gmail.com>
2015-08-30 15:59:41 -0600

    actualizado

--- Sets.java
+++ Sets.java
@@ -1,4 +1,25 @@
-
+import java.util.*;
 public class Sets {
-
+	private Set<Programador> st;
+	private Programador primero;
+	private Iterator ir;
+	
+	public class Sets(int tipo){
+		Iterator ir  = new Iterable();
+		if(tipo == 0){
+		}
+			st = new HashSet<Programador>();
+		if(tipo == 1){
+			st = new TreeSet<Programador>();
+		
+		}
+		
+		if(tipo == 2){
+			st = new LinkedHashSet<Programador>();
+		}
+	}
+	
+	
+	
+	
 }
